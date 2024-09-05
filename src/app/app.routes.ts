a12e214dcdbc77bcd081ad98f9aa6813afc544d7
mcalderon <mcalderon>
2024-09-05 15:01:07 -0500

    se mejoro el login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     title: 'Login',
     path: '',
     component: LoginComponent,
+  },
+  {
+    title: 'Login',
+    path: 'dashboard',
+    component: DashboardComponent,
   },
   { path: '**', component: NotfoundComponent , title: '404 - Not Found'},
 ];
