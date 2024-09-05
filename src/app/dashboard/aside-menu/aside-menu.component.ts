c7270aac4c7aade3a1300de4db0f8eadb9108344
mcalderon <mcalderon>
2024-09-05 16:30:28 -0500

    se agrego usuarios2

--- src/app/dashboard/aside-menu/aside-menu.component.ts
+++ src/app/dashboard/aside-menu/aside-menu.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-aside-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './aside-menu.component.html',
   styleUrl: './aside-menu.component.css'
 })
 export class AsideMenuComponent {
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    initFlowbite();
+  }
 
 }
