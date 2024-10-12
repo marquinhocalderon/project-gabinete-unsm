b6a6310f0924e993c227eabfe128e36c2e2fc2b1
mcalderon <mcalderon>
2024-10-11 19:10:28 -0500

    mejorando algunas cosas

--- src/app/modulos/gabinetes/gabinetes.component.ts
+++ src/app/modulos/gabinetes/gabinetes.component.ts
@@ -15,7 +15,7 @@ import { EstadoGlobalGuardarDatosService } from '../../core/guardardatos/estado-
 export class GabinetesComponent {
 
   constructor(private apiservicios: ApipeticionesService, private serviciosglobal: EstadoGlobalGuardarDatosService) { }
-
+  urlArchivoasver: string = import.meta.env.NG_APP_API + '/file/imagen/';
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -38,21 +38,34 @@ export class GabinetesComponent {
   }
 
 
-  urlArchivoasver: string = import.meta.env.NG_APP_API + '/file/imagen/';
+  valor: string = ''; // Define el tipo como string
+datos: any[] = [];
+datosFiltrados: any[] = []; // Para almacenar los resultados filtrados
+changeBuscador(event: any) {
+  this.valor = event.target.value.trim(); // Elimina espacios en blanco
 
-  datos: any[] = [];
+  // Filtra los datos según el valor ingresado
+  this.datosFiltrados = this.datos.filter(item => {
+    const nombreGabinete = item.nombre_gabinete ? item.nombre_gabinete.toLowerCase() : '';
+    const facultad = item.facultades && item.facultades.facultad ? item.facultades.facultad.toLowerCase() : '';
 
-  getdata(){
-    const url = import.meta.env.NG_APP_API + '/gabinetes';
-    this.apiservicios.getApi(url).subscribe({
-      next: (data: any) => {
-        console.log(data);
-        this.datos = data;
-      },
-      error: (error: any) => {
-        console.log(error); 
-      }
-    })
-  }
+    return nombreGabinete.includes(this.valor.toLowerCase()) || facultad.includes(this.valor.toLowerCase());
+  });
+}
+
+
+getdata() {
+  const url = import.meta.env.NG_APP_API + '/gabinetes';
+  this.apiservicios.getApi(url).subscribe({
+    next: (data: any) => {
+      console.log(data);
+      this.datos = data;
+      this.datosFiltrados = data; // Inicializa los datos filtrados con los datos originales
+    },
+    error: (error: any) => {
+      console.log(error);
+    }
+  });
+}
 
 }
