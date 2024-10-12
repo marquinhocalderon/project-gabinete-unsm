646880027c87f02397cefe561f5cc59befec02f0
mcalderon <mcalderon>
2024-10-11 19:55:16 -0500

    mejiorando datos

--- src/app/modulos/facultades/facultades.component.ts
+++ src/app/modulos/facultades/facultades.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { PostfacultadesComponent } from './postfacultades/postfacultades.component';
 import { ApipeticionesService } from '../../core/servicios/apipeticiones.service';
+import { PutfacultadesComponent } from "./putfacultades/putfacultades.component";
+import { TokensService } from '../../core/auth/tokens.service';
+import { EstadoGlobalGuardarDatosService } from '../../core/guardardatos/estado-global-guardar-datos.service';
 
 @Component({
   selector: 'app-facultades',
   standalone: true,
-  imports: [PostfacultadesComponent],
+  imports: [PostfacultadesComponent, PutfacultadesComponent],
   templateUrl: './facultades.component.html',
   styleUrl: './facultades.component.css'
 })
 export class FacultadesComponent {
 
-  constructor(private api: ApipeticionesService) { }
-
+  constructor(private api: ApipeticionesService, private token : TokensService, private datosglobales : EstadoGlobalGuardarDatosService) { }
 
   ngOnInit(): void {
       this.getData();
@@ -31,6 +33,19 @@ export class FacultadesComponent {
         console.error('Error al obtener datos:', error);
       }
     })
+  }
+
+
+  verModal: boolean = false;
+
+  mostrarModal(datos: any){
+    this.verModal = true;
+    this.datosglobales.setDatosServicioGlobal(datos);
+  }
+
+  cerrarModal(event: boolean){
+    console.log(event);
+    this.verModal = event;
   }
 
 
--- src/app/modulos/facultades/putfacultades/putfacultades.component.ts
+++ src/app/modulos/facultades/putfacultades/putfacultades.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import Swal from 'sweetalert2';
+import { ApipeticionesService } from '../../../core/servicios/apipeticiones.service';
+import { TokensService } from '../../../core/auth/tokens.service';
+import { EstadoGlobalGuardarDatosService } from '../../../core/guardardatos/estado-global-guardar-datos.service';
 
 @Component({
   selector: 'app-putfacultades',
@@ -8,5 +13,60 @@ import { Component } from '@angular/core';
   styleUrl: './putfacultades.component.css'
 })
 export class PutfacultadesComponent {
+  
+  constructor(private api: ApipeticionesService, private token : TokensService,private datosglobales : EstadoGlobalGuardarDatosService ) { }
+  @Output() estadoModal2 = new EventEmitter<boolean>();
+  id_usuario :any
+  datos : any;
+  ngOnInit(): void {
+    
+    initFlowbite();
+    this.datos = this.datosglobales.getDatosServicioGlobal(); 
+    this.id_usuario = this.token.decodeToken().sub;  
+    console.log(this.id_usuario);
+  }
 
+
+  loading:boolean = false;  
+  enviarDatos(e:any){
+    e.preventDefault();
+    this.loading = true;
+    
+    const formData = new FormData(e.target);
+
+    const formulario = {
+      facultad: formData.get('facultad'),
+      id_usuario: String(this.id_usuario)
+    }
+
+    const url = import.meta.env.NG_APP_API + '/facultades/' + this.datos.id;
+
+    this.api.patchApi(url, formulario).subscribe({
+      next: (data:any) => {
+        console.log(data);
+        this.loading = false;
+        Swal.fire({
+          title: 'Facultad Actualizada',
+          icon: 'success',
+          showCancelButton: false,
+          showConfirmButton: false,
+          timer: 1500
+        }).then(() => {
+          window.location.reload();
+        });
+      },
+      error: (error:any) => {
+        console.error('Error al enviar datos:', error);
+        this.loading = false;
+      }
+    })
+
+
+
+
+  }
+
+  cerrarModal(){
+    this.estadoModal2.emit(false);
+  }
 }
