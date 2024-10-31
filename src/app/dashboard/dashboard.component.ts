2e87051d368dbe05ea865d474615ad5aa31d8597
mcalderon <mcalderon>
2024-10-30 23:31:01 -0500

    se esta mejorando el tema de diseño dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AsideMenuComponent } from './aside-menu/aside-menu.component';
+import { ApipeticionesService } from '../core/servicios/apipeticiones.service';
+import { TokensService } from '../core/auth/tokens.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarMenuComponent,RouterOutlet, AsideMenuComponent],
+  imports: [NavbarMenuComponent,RouterOutlet, AsideMenuComponent,RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  constructor(private api: ApipeticionesService, private respuestaToken: TokensService) { }
 
-  constructor() { }
+  idUsuario: any;
+  modulosQuetieneUsuario: any = [];
 
   sidebarAbierto: boolean = false;
 
@@ -22,8 +26,26 @@ export class DashboardComponent {
     console.log('sidebarAbierto', this.sidebarAbierto);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const decodeToken = this.respuestaToken.decodeToken();
+    this.idUsuario = decodeToken.sub;
+    this.getModulosQueTieneUsuario();
     initFlowbite();
+  }
+
+
+
+  getModulosQueTieneUsuario(): void {
+    const urlUsuarios = import.meta.env.NG_APP_API + '/peticion/usuarios/' + this.idUsuario;
+
+    this.api.getApi(urlUsuarios).subscribe({
+      next: (data: any) => {
+        this.modulosQuetieneUsuario = data[0]?.modulos_para_mostrar_menu;
+      },
+      error: (error: any) => {
+        console.error('Error al obtener módulos del usuario:', error);
+      },
+    });
   }
 
 }
