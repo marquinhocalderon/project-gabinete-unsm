9f902bb413ad786ba8ab8867400584726ab39296
mcalderon <mcalderon>
2024-12-10 11:28:21 -0500

    mostrando

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,7 +20,7 @@ export class DashboardComponent {
   modulosQuetieneUsuario: any = [];
 
   sidebarAbierto: boolean = false;
-
+  tienePermisos: boolean = false;
   recogerRespuestaDelHijo(event: boolean) {
     this.sidebarAbierto = event;
     console.log('sidebarAbierto', this.sidebarAbierto);
@@ -34,6 +34,7 @@ export class DashboardComponent {
   }
 
 
+  datosMostarMenu: any 
 
   getModulosQueTieneUsuario(): void {
     const urlUsuarios = import.meta.env.NG_APP_API + '/peticion/usuarios/' + this.idUsuario;
@@ -41,11 +42,36 @@ export class DashboardComponent {
     this.api.getApi(urlUsuarios).subscribe({
       next: (data: any) => {
         this.modulosQuetieneUsuario = data[0]?.modulos_para_mostrar_menu;
+        this.datosMostarMenu = data[0]?.modulos_para_actualizar
+        console.log('datosMostarMenu', this.datosMostarMenu);
+        this.verificarPermisos();
       },
       error: (error: any) => {
         console.error('Error al obtener módulos del usuario:', error);
       },
     });
   }
+  verificarPermisos(): void {
+    // Encontrar el módulo 'seguridad'
+    const moduloSeguridad = this.datosMostarMenu.find((modulo: any) => modulo.nombre_modulo === 'seguridad');
+    
+    if (moduloSeguridad) {
+      // Encontrar el submódulo 'usuarios' dentro del módulo 'seguridad'
+      const subModuloUsuarios = moduloSeguridad.sub_modulos.find((sub_modulo: any) => sub_modulo.nombre_submodulo === 'usuarios');
+  
+      if (subModuloUsuarios) {
+        // Verificar si todos los permisos en el submódulo 'usuarios' tienen valor 'true'
+        this.tienePermisos = subModuloUsuarios.permisos.every((permiso: any) => permiso.value === true);
+      } else {
+        this.tienePermisos = false; // Si no se encuentra el submódulo 'usuarios', no tiene permisos
+      }
+    } else {
+      this.tienePermisos = false; // Si no se encuentra el módulo 'seguridad', no tiene permisos
+    }
+    
+    // Mostrar en la consola si tiene permisos en 'usuarios' dentro del módulo 'seguridad' o no
+    console.log('tienePermisos para usuarios dentro de seguridad:', this.tienePermisos); // Esto imprimirá true o 
+  }  
+  
 
 }
